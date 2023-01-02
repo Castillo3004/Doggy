9edc484a7d1ea42b01482ff9610ea7a1d5768975
Castillo3004 <castillolj3004@gmail.com>
2023-01-01 22:22:48 -0500

    Se agrego el archivo para temas

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ class AppTheme{
+
+  static const Color primary = Colors.brown;
+
+  static final ThemeData lightTheme = ThemeData.light().copyWith(
+
+    primaryColor: primary
+
+
+
+  );
+
+
+ }
