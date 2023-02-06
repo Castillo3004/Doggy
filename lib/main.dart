2e6c2e04ecf1380312d7410a81454b4d628c8a70
Castillo3004 <cacastillo34@utpl.edu.ec>
2023-02-06 17:18:58 -0500

    Se agregaron conexiones a Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,18 @@ import 'package:doggy/routes/app_routes.dart';
 import 'package:doggy/theme/app_theme.dart';
 import 'package:provider/provider.dart';
 
+// IMportaciones de Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MyApp());
+}
 
 
 class MyApp extends StatelessWidget{
